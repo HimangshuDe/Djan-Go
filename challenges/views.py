2fa1bc8886ac42463588adaa9650a9eba6f913a1
HimangshuDe <dehimangshu03@gmail.com>
2023-12-20 20:05:26 +0530

    Templates in Challenge app
    Added templates in challenge app
    
    Modified some view logics accordingly.
    
    Modified settings to render the templates properly.

--- challenges/views.py
+++ challenges/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 from django.urls import reverse
+from django.template.loader import render_to_string
 
 # Create your views here.
 
@@ -44,9 +45,9 @@ def monthly_challenges_by_number(request, month:int):
 
 
 def monthly_challenge(request, month:str):
-    try:
+    # try:
         challenge_text = monthly_challenges[month]
-        response_data = f"<h1>{challenge_text}</h1>"
+        response_data = render_to_string("challenges/challenge.html")
         return HttpResponse(response_data)
-    except:
-        return HttpResponseNotFound("This month is not found!")
+    # except:
+        # return HttpResponseNotFound("This month is not found!")
