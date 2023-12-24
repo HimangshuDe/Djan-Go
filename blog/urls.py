43b56edcaa4cc26811a50886767bb19262390e5d
HimangshuDe <dehimangshu03@gmail.com>
2023-12-24 11:44:32 +0530

    Basic structure done!
    Added views for web logic
    
    Added routes for previewing the pages
    
    Added templates for rendering HTML content
    
    Added static files(css only) for making HTML content visually appealing!

---
+++ blog/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from blog.views import starting_page,posts, post_detail
+
+urlpatterns = [
+    path("", starting_page, name="starting-page"),
+    path("posts/", posts, name="posts-page"),
+    path("posts/<slug:slug>",post_detail, name="post-detail-page") #posts/my-first-postpost_details
+]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def starting_page(request):
+    return render(request, "blog/starting-page.html")
+
+def posts(request):
+    return render(request, "blog/posts.html")
+
+def post_detail(request, slug):
+    return render(request, "blog/post-detail-page.html")
