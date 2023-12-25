db17fab4948b9e9e0c144ae5d6e709dfce302079
Himangshu De <120447873+HimangshuDe@users.noreply.github.com>
2023-12-25 10:57:55 +0530

    Static Images and Templates
    Added static files(css, images) for respective html templates.
    
    Modified html templates for all posts page.
    
    Modified file path in views.

--- blog/views.py
+++ blog/views.py
@@ -6,7 +6,7 @@ def starting_page(request):
     return render(request, "blog/starting-page.html")
 
 def posts(request):
-    return render(request, "blog/posts.html")
+    return render(request, "blog/all-posts.html")
 
 def post_detail(request, slug):
     return render(request, "blog/post-detail-page.html")
