aeb06780707aa3eed567b0b78267cca2ec371b02
Himangshu De <120447873+HimangshuDe@users.noreply.github.com>
2024-01-08 19:26:35 +0530

    Completed working on Single Post html page.

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from blog.views import starting_page,posts, post_detail
+from blog.views import starting_page,all_posts, post_detail
 
 urlpatterns = [
     path("", starting_page, name="starting-page"),
-    path("posts/", posts, name="posts-page"),
+    path("posts/", all_posts, name="posts-page"),
     path("posts/<slug:slug>",post_detail, name="post-detail-page") #posts/my-first-postpost_details
 ]
