da3748aa73f52360ca4c3300d80a249d7639c92b
Himangshu De <dehimangshu03@gmail.com>
2024-01-30 22:43:08 +0530

    Styling Added Styling to raw HTML

--- book_outlet/models.py
+++ book_outlet/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
@@ -8,6 +9,12 @@ class Book(models.Model):
     rating = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
     author = models.CharField(null=True, max_length=100)
     is_bestselling = models.BooleanField(default=False)
+
+    def get_absolute_url(self):
+        """
+        This will not work properly if any field is not defined in the Book Model Class.
+        """
+        return reverse("book-detail", args=[str(self.id)])
 
     def __str__(self):
         return f"{self.title} ({self.rating})"
