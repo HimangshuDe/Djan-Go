3290d8463039f1b23c05a35b747adb5e3cabbc60
Himangshu De <dehimangshu03@gmail.com>
2024-02-03 20:29:39 +0530

    Models and Relations
    > Added Models and relations
    > Modified gitignore

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.core.validators import MinLengthValidator
 
 # Create your models here.
+class Author(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email_address = models.EmailField()
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+class Post(models.Model):
+    title = models.CharField(max_length=150)
+    excerpt = models.CharField(max_length=200)
+    image_name = models.CharField(max_length=100)
+    date = models.DateField(auto_now=True)
+    slug = models.SlugField(unique=True, db_index=True)
+    content = models.TextField(validators=[MinLengthValidator(15)])
+    author = models.ForeignKey(Author, on_delete=models.SET_NULL, null=True, related_name='posts')
+
+    def __str__(self):
+        return self.title
+    
