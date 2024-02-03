08c45b1a3b08ce9e5c0b7f9bc2992b26df4a8860
Himangshu De <dehimangshu03@gmail.com>
2024-02-03 19:59:02 +0530

    Changes Made
    > Added new model 'Country' and registered it with admin files.
    
    > Minor changes on the index.html and index.css

--- book_outlet/models.py
+++ book_outlet/models.py
@@ -5,6 +5,17 @@ from django.utils.text import slugify
 
 # Create your models here.
 
+class Country(models.Model):
+    name = models.CharField(max_length=80)
+    code = models.CharField(max_length=2)
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name_plural = "Countries"
+
+
 class Address(models.Model):
     street = models.CharField(max_length=80)
     postal_code = models.CharField(max_length=6)
@@ -38,6 +49,7 @@ class Book(models.Model):
     author = models.ForeignKey(Author, on_delete=models.CASCADE, null=True, related_name="books")
     is_bestselling = models.BooleanField(default=False)
     slug = models.SlugField(default="", null=False, db_index=True)
+    published_countries = models.ManyToManyField(Country, related_name="books")
 
     def get_absolute_url(self):
         """
