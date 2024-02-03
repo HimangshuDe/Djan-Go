503a8a3d9d22f55f8e0b67e05a85c1604a3ed1cb
Himangshu De <dehimangshu03@gmail.com>
2024-02-03 10:33:51 +0530

    Changes Made
    > Added the Address and Author models in the book_outlet/models.py file.
    > Defined the full_addr method in the Address model to return the full address.
    > Defined the __str__ method in the Address model to return a string representation of the full address.
    > Defined the full_name method in the Author model, but the implementation is incomplete.

--- book_outlet/admin.py
+++ book_outlet/admin.py
@@ -1,9 +1,18 @@
 from django.contrib import admin
-from .models import Book
+from .models import Book, Author, Address
 
 # Register your models here.
 
 class BookAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ["title"]}
+    list_filter = ["rating", "author"]
+    list_display = ["title", "author"]
+
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ["first_name", "last_name"]
+
 
 admin.site.register(Book, BookAdmin)
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(Address)
