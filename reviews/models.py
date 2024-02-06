e762f6ad6eb2817a27d95c4ef583901fb47387f2
Himangshu De <dehimangshu03@gmail.com>
2024-02-06 10:57:10 +0530

    ModelForms
    > Introduced ModelForms.
    > Form fields are rendered according to the model fields so declared in models.py
    > Adjusted view logic w.r.t forms.py
    
    ** Migrations added for field alteration. [000*.py]

--- reviews/models.py
+++ reviews/models.py
@@ -4,5 +4,5 @@ from django.db import models
 # Create your models here.
 class Review(models.Model):
     user_name = models.CharField(max_length=100)
-    review_text = models.TextField(max_length=200)
+    review_text = models.TextField()
     rating = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
