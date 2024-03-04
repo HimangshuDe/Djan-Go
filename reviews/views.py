d456776a6c7fe4b422c1e6810e1924217bfe261b
HimangshuDe <dehimangshu03@gmail.com>
2024-03-04 10:50:05 +0530

    Major Changes
    > Updated View logic for displaying taking reviews, displaying all of them, and displaying a particular review
    
    > Updated routes accordingly.
    
    ** Main Code files are in reviews/

--- reviews/views.py
+++ reviews/views.py
@@ -1,8 +1,12 @@
+from typing import Any
+from django.db.models.query import QuerySet
 from django.http import HttpResponseRedirect
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import View
+from django.views.generic import TemplateView, ListView, DetailView
 
 from reviews.forms import ReviewForm
+from reviews.models import Review
 
 # Create your views here.
 
@@ -21,5 +25,28 @@ class ReviewView(View):
         return render(request, "reviews/review.html", {"form": form})
 
 
-def thank_you(request):
-    return render(request, "reviews/thank_you.html")
+class ThankYouView(TemplateView):
+    template_name = "reviews/thank_you.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["message"] = "This works!"
+        return context
+
+
+class ReviewListView(ListView):
+    template_name = "reviews/review_list.html"
+    model = Review
+    context_object_name = "reviews"
+
+    # def get_queryset(self):
+    #     base_query = super().get_queryset()
+    #     data = base_query.filter(rating__gt=4)
+    #     return data
+
+
+class SingleReviewView(DetailView):
+    template_name = "reviews/single_review.html"
+    model = Review
+    # context_object_name = "review"
+    # NOTE: by default django use model name(all in lowercase) as context_object_name(which exposes variable in Django templates)
