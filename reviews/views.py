dc2e732362209ff791dc168966a27b681b0a1499
HimangshuDe <dehimangshu03@gmail.com>
2024-03-04 18:17:09 +0530

    Updated views
    > removed unused import

--- reviews/views.py
+++ reviews/views.py
@@ -1,7 +1,6 @@
 from typing import Any
-from django.db.models.query import QuerySet
 from django.http import HttpResponseRedirect
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render
 from django.views import View
 from django.views.generic import TemplateView, ListView, DetailView
 
