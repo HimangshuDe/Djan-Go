42dee6f69047b8ed31e4ff5f2b82fc9817ff8dd3
HimangshuDe <dehimangshu03@gmail.com>
2024-03-04 20:16:00 +0530

    File uploads in action.
    > Adjusted view logics for handling uploads
    
    > Created model fields for storing uploaded files
    
    > Updated settings
    
    ** Minor **
    -> Created forms for uploading files
    
    -> Modified HTML file(s) for rendering forms so created
    
    -> Modified gitignore

--- profiles/views.py
+++ profiles/views.py
@@ -1,12 +1,23 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.views import View
+
+from .forms import ProfileForm
+from .models import UserProfile
 
 # Create your views here.
 
 
 class CreateProfileView(View):
     def get(self, request):
-        return render(request, "profiles/create_profile.html")
+        return render(request, "profiles/create_profile.html", {"form": ProfileForm()})
 
     def post(self, request):
-        pass
+        submitted_form = ProfileForm(request.POST, request.FILES)
+
+        if submitted_form.is_valid():
+            profile = UserProfile(image=request.FILES["user_image"])
+            profile.save()
+            return HttpResponseRedirect("/profiles")
+
+        return render(request, "profiles/create_profile.html", {"form": submitted_form})
