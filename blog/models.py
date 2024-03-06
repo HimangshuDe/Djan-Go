f50ba9be42943ba2722b99333d93e7d65804caed
HimangshuDe <dehimangshu03@gmail.com>
2024-03-06 18:58:37 +0530

    Added file uploads for this project.

--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,7 @@ from django.core.validators import MinLengthValidator
 
 # Create your models here.
 
+
 class Tag(models.Model):
     caption = models.CharField(max_length=20)
 
@@ -18,16 +19,18 @@ class Author(models.Model):
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
 
+
 class Post(models.Model):
     title = models.CharField(max_length=150)
     excerpt = models.CharField(max_length=200)
-    image_name = models.CharField(max_length=100)
+    image = models.ImageField(upload_to="posts", null=True)
     date = models.DateField(auto_now=True)
     slug = models.SlugField(unique=True, db_index=True)
     content = models.TextField(validators=[MinLengthValidator(15)])
-    author = models.ForeignKey(Author, on_delete=models.SET_NULL, null=True, related_name='posts')
+    author = models.ForeignKey(
+        Author, on_delete=models.SET_NULL, null=True, related_name="posts"
+    )
     tags = models.ManyToManyField(Tag)
 
     def __str__(self):
         return self.title
-    
