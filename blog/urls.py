eafa0b0d4bf3cd0b2db56e37ab80337e5702766e
HimangshuDe <dehimangshu03@gmail.com>
2024-03-07 11:15:51 +0530

    Read Later in action
    > Added read later for posts to read them later.
    
    > Displaying some html conditionally
    
    > Updated some styling
    
    > Adjusted some view logic
    
    > Updated routes

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from blog.views import StartingPageView, AllPostsView, SinglePostView
+from blog.views import ReadLaterView, StartingPageView, AllPostsView, SinglePostView
 
 urlpatterns = [
     path("", StartingPageView.as_view(), name="starting-page"),
@@ -7,4 +7,5 @@ urlpatterns = [
     path(
         "posts/<slug:slug>", SinglePostView.as_view(), name="post-detail-page"
     ),  # posts/my-first-postpost_details
+    path("read-later", ReadLaterView.as_view(), name="read-later"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -47,14 +47,24 @@ class AllPostsView(ListView):
 
 class SinglePostView(View):
 
+    def is_stored_post(self, request, post_id):
+        stored_post = request.session.get("stored_posts")
+        if stored_post is None:
+            is_saved_for_later = False
+        else:
+            is_saved_for_later = post_id in stored_post
+        return is_saved_for_later
+
     # GET request
     def get(self, request, slug):
         post = Post.objects.get(slug=slug)
+
         context = {
             "post": post,
             "post_tags": post.tags.all(),
             "all_comment": post.comments.all().order_by("-id"),
             "comment_form": CommentForm(),
+            "is_saved_for_later": self.is_stored_post(request, post.id),
         }
         return render(request, "blog/post-detail.html", context)
 
@@ -73,6 +83,7 @@ class SinglePostView(View):
             "post_tags": post.tags.all(),
             "all_comment": post.comments.all().order_by("-id"),
             "comment_form": comment_form,
+            "is_saved_for_later": self.is_stored_post(request, post.id),
         }
         return render(request, "blog/post-detail.html", context)
 
@@ -84,3 +95,38 @@ class SinglePostView(View):
 #     "blog/post-detail.html",
 #     {"post": identified_post, "post_tags": identified_post.tags.all()},
 # )
+
+
+class ReadLaterView(View):
+    # GET request
+    def get(self, request):
+        stored_post = request.session.get("stored_posts")
+        context = {}
+        if stored_post is None or len(stored_post) == 0:
+            context["posts"] = []
+            context["has_posts"] = False
+        else:
+            posts = Post.objects.filter(id__in=stored_post)
+            context["posts"] = posts
+            context["has_posts"] = True
+
+        return render(request, "blog/read-later.html", context)
+
+    #  POST request
+    def post(self, request):
+        stored_posts = request.session.get("stored_posts")
+
+        if stored_posts is None:
+            stored_posts = []
+
+        post_id = int(request.POST["post_id"])
+
+        if post_id not in stored_posts:
+            stored_posts.append(post_id)
+
+        else:
+            stored_posts.remove(post_id)
+
+        request.session["stored_posts"] = stored_posts
+
+        return HttpResponseRedirect(reverse("starting-page"))
